d909ac229b54b2e331f83e2afe83a8a67dc78c3d
Harry Beggs <hjbeggs99@gmail.com>
2019-08-19 01:50:13 +0100

    Code working just wont keep headers

--- scrape.py
+++ scrape.py
@@ -1,30 +1,48 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 data = []
 source = requests.get('https://www.newegg.com/global/uk-en/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=1582767').text
 
 soup = BeautifulSoup(source, 'lxml')
 
-
+file = 'data.csv'
 data = soup.find_all('div',{'class':'item-container'})
-for element in data:
-	try:
-		eBrand = element.find('div', 'item-info').div.a.img['title']
-	except Exception as e:
-		eBrand = 'NA'
-	else:
-		pass
-	finally:
-		pass
+with open(file, 'a') as f:
+	writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	for element in data:
+		try: #some items dont have a brand
+			eBrand = element.find('div', 'item-info').div.a.img['title']
+		except Exception as e:
+			eBrand = 'N/A'
+		else:
+			pass
+		finally:
+			pass
 
-	eName = element.find('a',{'class':'item-title'}).text
-	ePrice = element.find('li',{'class':'price-current'})
-	try:
-		eDiscount = element.find('span',{'class':'price-save-percent'}).text
-	except Exception as e:
-		eDiscount = 'None'
-	else:
-		pass
-	finally:
-		pass
-	print(eBrand, '\n', eName, '\n', '£', ePrice.strong.text, ePrice.sup.text, '\n', eDiscount)
+		eName = element.find('a',{'class':'item-title'}).text
+		containPrice = element.find('li',{'class':'price-current'})
+
+		try: #some prices arent available until you add the item to your cart
+			ePrice = containPrice.strong.text
+			eDecimal = containPrice.sup.text
+			eTotal = '£' + ePrice + eDecimal
+		except Exception as e:
+			eTotal = 'N/A'
+		else:
+			pass
+		finally:
+			pass
+		
+		try: #not all items have a discount
+			eDiscount = element.find('span',{'class':'price-save-percent'}).text
+		except Exception as e:
+			eDiscount = 'None'
+		else:
+			pass
+		finally:
+			pass
+		
+		writer.writerow([eBrand, eName, eTotal, eDiscount])
+		print(eBrand, '\n', eName, '\n', eTotal, '\n', eDiscount)
+
