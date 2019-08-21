0019fdfbd8965ab551a89bd38f23f2d0b932a408
Harry Beggs <hjbeggs99@gmail.com>
2019-08-21 15:34:14 +0100

    I can no longer access the website

--- scrape.py
+++ scrape.py
@@ -2,48 +2,54 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 data = []
-source = requests.get('https://www.newegg.com/global/uk-en/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=1582767').text
-
-soup = BeautifulSoup(source, 'lxml')
 
 file = 'data.csv'
-data = soup.find_all('div',{'class':'item-container'})
+
 with open(file, 'a') as f:
 	writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 	writer.writerow(['Brand', 'Name', 'Price', 'Discount'])
-	for element in data:
-		try: #some items dont have a brand
-			eBrand = element.find('div', 'item-info').div.a.img['title']
-		except Exception as e:
-			eBrand = 'N/A'
-		else:
-			pass
-		finally:
-			pass
-
-		eName = element.find('a',{'class':'item-title'}).text
-		containPrice = element.find('li',{'class':'price-current'})
-
-		try: #some prices arent available until you add the item to your cart
-			ePrice = containPrice.strong.text
-			eDecimal = containPrice.sup.text
-			eTotal = '£' + ePrice + eDecimal
-		except Exception as e:
-			eTotal = 'N/A'
-		else:
-			pass
-		finally:
-			pass
+	for pg in range(0, 16):
+		source = requests.get('https://www.newegg.com/global/uk-en/Desktop-Graphics-Cards/SubCategory/ID-48/Page-' + str(pg) + '?Tid=1582767').text
+		print(source)
+		soup = BeautifulSoup(source, 'lxml')
+		data = soup.find_all('div',{'class':'item-container'})
 		
-		try: #not all items have a discount
-			eDiscount = element.find('span',{'class':'price-save-percent'}).text
-		except Exception as e:
-			eDiscount = 'None'
-		else:
-			pass
-		finally:
-			pass
-		
-		writer.writerow([eBrand, eName, eTotal, eDiscount])
-		print(eBrand, '\n', eName, '\n', eTotal, '\n', eDiscount)
+		for element in data:
+			try: #some items dont have a brand
+				eBrand = element.find('div', 'item-info').div.a.img['title']
+			except Exception as e:
+				eBrand = 'N/A'
+			else:
+				pass
+			finally:
+				pass
+
+			eName = element.find('a',{'class':'item-title'}).text
+			containPrice = element.find('li',{'class':'price-current'})
+
+			try: #some prices arent available until you add the item to your cart
+				ePrice = containPrice.strong.text
+				eDecimal = containPrice.sup.text
+				eTotal = '£' + ePrice + eDecimal
+			except Exception as e:
+				eTotal = 'N/A'
+			else:
+				pass
+			finally:
+				pass
+			
+			try: #not all items have a discount
+				eDiscount = element.find('span',{'class':'price-save-percent'}).text
+			except Exception as e:
+				eDiscount = 'None'
+			else:
+				pass
+			finally:
+				pass
+			
+			writer.writerow([eBrand, eName, eTotal, eDiscount])
+			print(eBrand, '\n', eName, '\n', eTotal, '\n', eDiscount)
+			
+			#clean up
+
 
